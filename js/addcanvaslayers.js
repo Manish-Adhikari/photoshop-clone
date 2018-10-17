eed1e64bbd0cd055c8e27bfec6969e9034bc4b7e
Manish <jon.manish.adh@gmail.com>
2018-10-17 20:36:26 +0545

    layers displayed

--- js/addcanvaslayers.js
+++ js/addcanvaslayers.js
@@ -1,6 +1,7 @@
 var addLayer = document.getElementsByClassName('plus-button')[0];
 var removeLayer = document.getElementsByClassName('minus-button')[0];
 var myCanvas;
+var layerIndex = 0;
 var canvasArray = [];
 var lastElement;
 var brushColor;
@@ -17,9 +18,10 @@ var colorArray = ['red',
 addLayer.onclick = function() {
     console.log('layer added');
     brushColor = colorArray[Math.floor(Math.random()*colorArray.length)];
-    console.log(brushColor);
-    myCanvas = new createCanvas(brushColor);
+    console.log('Brush Color: ' + brushColor);
+    myCanvas = new createCanvas(brushColor,layerIndex);
     canvasArray.push(myCanvas);
+    layerIndex++;
     console.log(canvasArray);
 }
 
@@ -27,6 +29,8 @@ removeLayer.onclick = function() {
     console.log('layer removed');
     lastElement = canvasArray[canvasArray.length - 1];
     lastElement.canvas.remove();
+    lastElement.layerDiv.remove();
     canvasArray.splice(-1, 1);
     console.log(canvasArray);
+    layerIndex--;
 }
--- js/createcanvas.js
+++ js/createcanvas.js
@@ -1,6 +1,7 @@
 var canvasWrapper = document.getElementsByClassName('canvas-wrapper')[0];
+var layersWrapper = document.getElementsByClassName('layers-wrapper')[0];
 
-function createCanvas(brushColor) {
+function createCanvas(brushColor,layerIndex) {
     this.canvas = document.createElement('canvas');
     this.canvasWrapper = canvasWrapper;
     this.canvasWrapper.appendChild(this.canvas);
@@ -12,12 +13,33 @@ function createCanvas(brushColor) {
     this.flag = false;
     this.context = this.canvas.getContext('2d');
     this.context.lineWidth = 20;
-    this.event;
+    this.layerIndex= layerIndex;
+   
+    this.layerDiv = document.createElement('div');
+    this.layerDiv.style.height = '30px';
+    this.layerDiv.style.width = '100%';
+    this.layerDiv.style.background = 'lightgrey';
+    this.textHere = document.createTextNode('Layer '+this.layerIndex);
+    this.layerDiv.appendChild(this.textHere);
+    this.layerDiv.style.marginBottom = '3px';
+    this.layerDiv.style.textAlign = 'center';
+    this.layerDiv.style.paddingTop = '10px';
+    layersWrapper.appendChild(this.layerDiv);
 
     that = this;
 
+    (function(myLayer,myCanvas,index){
+        myLayer.onclick = function() {
+        myLayer.style.border = '1px solid black';
+        myCanvas.style.zIndex = '1';
+        myCanvas.style.position = 'absolute';
+        console.log('iffe fired ' + index);
+         }
+    })(this.layerDiv,this.canvas,this.layerIndex);
+ 
+
     this.plotPoints = function(event) {
-        if (this.flag) {
+        if (that.flag) {
             that.context.fill();
             that.context.fillStyle = that.brushColor;
             that.context.lineTo(event.offsetX, event.offsetY)
@@ -33,12 +55,12 @@ function createCanvas(brushColor) {
     }
 
     this.enableFlag = function(event) {
-        this.flag = true;
+        that.flag = true;
         that.plotPoints(event);
     }
 
     this.disableFlag = function() {
-        this.flag = false;
+        that.flag = false;
         that.context.beginPath();
     }
 
